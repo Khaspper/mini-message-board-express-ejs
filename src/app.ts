2be20b5df1656d85cfa5ea718659d714f3612568
Mark <brandonnarciso24@gmail.com>
2025-08-05 18:57:52 -0700

    chore: add basic messages and views folder

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,33 @@ import express from "express";
 import path from "node:path";
 import indexRouter from "./routes";
 import newMessageRouter from "./routes/new-message";
+
 const app = express();
+const PORT = 3000;
+
+// Variables
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Gumball",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Darwin",
+    added: new Date(),
+  },
+];
+
+// Routers
 app.use("/", indexRouter);
 app.use("/new", newMessageRouter);
 
+// ejs
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const PORT = 3000;
-
+// Server
 app.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`);
 });
