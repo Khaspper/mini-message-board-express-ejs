7c5bccf0ad8400669c537d84fc949af677ddd879
Mark <brandonnarciso24@gmail.com>
2025-08-05 18:54:47 -0700

    chore: add basic routing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import path from "node:path";
-
+import indexRouter from "./routes";
+import newMessageRouter from "./routes/new-message";
 const app = express();
+app.use("/", indexRouter);
+app.use("/new", newMessageRouter);
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
